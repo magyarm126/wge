7256d56082353ba6cf57b020f54f164f9e4a1052
Magyar Máté <magyarm126>
2023-11-26 05:21:13 +0000

    Initial commit

---
+++ src/main/kotlin/hu/matemagyar/wge/Application.kt
@@ -0,0 +1,7 @@
+package hu.matemagyar.wge
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/kotlin/hu/matemagyar/wge/WgeTest.kt
@@ -0,0 +1,19 @@
+package hu.matemagyar.wge
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class WgeTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
