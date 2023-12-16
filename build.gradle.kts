9452f4bc08cb8099fa4045139d06dc1d90b9a517
Magyar Máté <36172841+magyarm126@users.noreply.github.com>
2023-12-16 01:16:25 +0100

    Added frontend gradle tasks (#6)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.gradle.node.npm.task.NpmTask
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.20"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.9.20"
@@ -5,6 +7,8 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.micronaut.application") version "4.2.0"
     id("io.micronaut.aot") version "4.2.0"
+    id("java")
+    id("com.github.node-gradle.node") version "3.0.1"
 }
 
 version = "0.1"
@@ -57,6 +61,78 @@ micronaut {
         optimizeNetty.set(true)
     }
 }
+// Node.js and NPM configuration
+node {
+    version = "21.4.0"
+    download = true
+}
 
+val commonNpmTask: NpmTask.() -> Unit = {
+    workingDir = file("frontend")
+}
 
+tasks {
+    val flagFile = layout.buildDirectory.file("cleanNodeModulesFlag.txt")
 
+    // Defining a task to install npm dependencies
+    val npmInstallAngular by creating(NpmTask::class) {
+        group = "Frontend"
+        commonNpmTask()
+        inputs.file("frontend/package.json")
+        outputs.file("frontend/package-lock.json")
+        args.set(listOf("install"))
+    }
+
+    // Angular build task
+    val ngBuild by creating(NpmTask::class) {
+        group = "Frontend"
+        dependsOn(npmInstallAngular)
+        commonNpmTask()
+        args.set(listOf("run", "build","--prod"))
+    }
+
+    // Angular serve task
+    val ngServe by creating(NpmTask::class) {
+        group = "Frontend"
+        dependsOn(npmInstallAngular)
+        commonNpmTask()
+        args.set(listOf("start"))
+    }
+
+    val killEsbuild by registering(Exec::class) {
+        group = "Frontend"
+        commandLine("cmd", "/c", "tasklist | find /i \"esbuild.exe\" && taskkill /F /IM esbuild.exe || echo Process not found, moving on")
+        isIgnoreExitValue = true
+    }
+
+    val killEsbuildLinux by registering(Exec::class) {
+        group = "Frontend"
+        commandLine("bash", "-c", "pkill -f esbuild || true") //might not work
+    }
+
+    val cleanNodeModules by registering(Delete::class) {
+        group = "Frontend"
+        description = "Clean the node modules folder"
+        delete("frontend/node_modules")
+        doLast {
+            println("Cleaned node_modules directory")
+            flagFile.get().asFile.writeText("cleaned")
+        }
+    }
+
+    val cleanNodeDist by registering(Delete::class) {
+        group = "Frontend"
+        description = "Clean the node dist folder"
+        delete("frontend/dist")
+        doLast {
+            println("Cleaned dist directory")
+        }
+    }
+
+    npmInstallAngular.mustRunAfter(cleanNodeModules)
+    npmInstallAngular.doLast {
+        flagFile.get().asFile.delete()
+    }
+
+    cleanNodeModules.get().dependsOn(killEsbuild)
+}
