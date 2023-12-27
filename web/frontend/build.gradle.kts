c0e1d55f59b1be6a4446bc6ebf6ab08003a31f7e
Magyar Máté <magyarmate126@gmail.com>
2023-12-27 23:41:51 +0100

    Added frontend as separate subproject

---
+++ web/frontend/build.gradle.kts
@@ -0,0 +1,44 @@
+import com.github.gradle.node.npm.task.NpxTask
+
+plugins {
+    id("com.github.node-gradle.node") version "3.0.1"
+    id("java")
+}
+
+version = "0.1"
+group = "hu.matemagyar.wge"
+
+// Node.js and NPM configuration
+node {
+    version = "21.4.0"
+    download = true
+}
+
+tasks {
+    val buildAngularApp by creating(NpxTask::class) {
+        group = "frontend"
+        dependsOn(npmInstall)
+        command = "ng"
+        args.set(listOf("build", "--configuration", "production"))
+        inputs.file("package.json")
+        outputs.file("package-lock.json")
+        inputs.dir(fileTree("node_modules").exclude(".cache"))
+        outputs.dir("dist")
+        doLast{
+            println("Built frontend distribution!")
+        }
+    }
+
+    processResources{
+        dependsOn(buildAngularApp)
+    }
+
+    clean {
+        delete("../frontend/node_modules")
+        delete("../frontend/dist")
+        delete("../frontend/.angular")
+        doLast{
+            println("Cleaned frontend caches!")
+        }
+    }
+}
