e725a25203528bbb615ad245c40179b35f3127c9
Magyar Máté <magyarmate126@gmail.com>
2023-12-29 01:36:31 +0100

    Added window opening with windows api, using the official Microsoft docs. https://learn.microsoft.com/en-us/windows/win32/learnwin32/windows-hello-world-sample

--- native/client/src/main/cpp/main.cpp
+++ native/client/src/main/cpp/main.cpp
@@ -1,9 +1,80 @@
-#include <iostream>
-#include <d3d12.h>
+#ifndef UNICODE
+#define UNICODE
+#endif
 
-int main() {
-    std::cout << "Hello, Client!" << std::endl;
-    constexpr D3D12_FORMAT_SUPPORT2 testEnum = D3D12_FORMAT_SUPPORT2_TILED;
-    std::cout << testEnum << std::endl;
+#include <windows.h>
+#pragma comment(lib,"user32.lib")
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+
+    // Register the window class.
+    const wchar_t CLASS_NAME[]  = L"Native wge client";
+
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Web Game Engine - Native C++ Client",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+        );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        return 0;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // All painting occurs here, between BeginPaint and EndPaint.
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
