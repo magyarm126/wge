0e27974f3361d2b6a36d6c3798e6e6f3045262d7
Magyar Máté <magyarmate126@gmail.com>
2024-01-01 23:36:02 +0100

    Added d3dx12.h helper header

--- native/client/include/stdafx.h
+++ native/client/include/stdafx.h
@@ -11,8 +11,10 @@
 #include <dxgi1_6.h>
 #include <D3Dcompiler.h>
 #include <DirectXMath.h>
+#include <d3dx12.h>
 
 #include <string>
 #include <wrl.h>
 
 using namespace Microsoft::WRL;
+using namespace DirectX;
