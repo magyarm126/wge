0adb7d2e3bee84e044d772d15ab68ce7e749e64b
Magyar Máté <magyarmate126@gmail.com>
2024-01-21 00:43:37 +0100

    Added User controller test

---
+++ web/backend/src/test/kotlin/hu/matemagyar/wge/controller/UserControllerTest.kt
@@ -0,0 +1,32 @@
+package hu.matemagyar.wge.controller
+
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+@MicronautTest
+class UserControllerTest {
+
+    @Inject
+    @Client("/")
+    lateinit var client: HttpClient
+
+    @Test
+    fun getUser() {
+        val request: HttpRequest<Any> = HttpRequest.GET("/user/1")
+        val body = client.toBlocking().retrieve(request)
+        assertNotNull(body)
+    }
+
+    @Test
+    fun getCurrentUser() {
+        val request: HttpRequest<Any> = HttpRequest.GET("/current-user")
+        val body = client.toBlocking().retrieve(request)
+        assertNotNull(body)
+    }
+}
