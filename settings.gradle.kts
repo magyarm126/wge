b4f732bc096a62da89b8ddbb66fc4e0c8ad78d3d
Magyar Máté <magyarmate126@gmail.com>
2024-03-03 21:49:18 +0100

    Fixed gradle kotlin false positive plugin problem.
    https://youtrack.jetbrains.com/issue/KT-64210/False-positive-warning-that-Kotlin-Gradle-plugin-was-loaded-multiple-times-in-different-subprojects-is-displayed

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,13 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.kotlin.jvm") version "1.9.21" apply(false)
-        id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply(false)
-        id("io.micronaut.library") version "4.2.0" apply(false)
-        id("org.jetbrains.kotlin.plugin.allopen") version "1.9.21" apply(false)
-        id("org.jetbrains.dokka") version "1.9.10" apply (false)
-        id("com.github.johnrengelman.shadow") version "8.1.1" apply(false)
-        id("io.micronaut.application") version "4.2.0" apply(false)
-        id("io.micronaut.aot") version "4.2.0" apply(false)
+        id("org.jetbrains.kotlin.jvm") version "1.9.21"
+        id("com.google.devtools.ksp") version "1.9.21-1.0.16"
+        id("io.micronaut.library") version "4.2.0"
+        id("org.jetbrains.kotlin.plugin.allopen") version "1.9.21"
+        id("org.jetbrains.dokka") version "1.9.10"
+        id("com.github.johnrengelman.shadow") version "8.1.1"
+        id("io.micronaut.application") version "4.2.0"
+        id("io.micronaut.aot") version "4.2.0"
     }
 }
 
