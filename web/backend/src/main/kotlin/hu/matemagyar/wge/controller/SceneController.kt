68d24558817b41696005eaa82d91bed0a0382d33
Magyar Máté <magyarmate126@gmail.com>
2024-03-10 07:25:16 +0100

    Java code generated

---
+++ web/backend/src/main/kotlin/hu/matemagyar/wge/controller/SceneController.kt
@@ -0,0 +1,15 @@
+package hu.matemagyar.wge.controller
+
+import hu.matemagyar.wge.model.Scene.SceneDto
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+@Controller
+class SceneController {
+
+    @Get("/scene")
+    fun getScene(): SceneDto {
+        println("Scene called")
+        return SceneDto.newBuilder().setId(1).setName("testScene").build()
+    }
+}
--- web/middleware/build.gradle.kts
+++ web/middleware/build.gradle.kts
@@ -1,8 +1,11 @@
+import com.google.protobuf.gradle.id
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("com.google.devtools.ksp")
     id("io.micronaut.minimal.library")
     id("org.jetbrains.dokka")
+    id("com.google.protobuf") version "0.9.4"
 }
 
 version = "0.1"
@@ -11,8 +14,50 @@ group = "hu.matemagyar.wge"
 dependencies {
     ksp("io.micronaut.serde:micronaut-serde-processor")
     compileOnly("io.micronaut.serde:micronaut-serde-api")
+
+    //implementation("com.google.protobuf:protoc:21.0-rc-1")
+    protobuf(files("../../common/protobuf"))
+    implementation("io.micronaut.grpc:micronaut-grpc-runtime")
 }
 
 java {
     sourceCompatibility = JavaVersion.toVersion("21")
+}
+
+protobuf {
+    protoc {
+        //artifact = "com.google.protobuf:protoc:21.0-rc-1"
+        artifact = "com.google.protobuf:protoc:3.6.1"
+    }
+
+    plugins {
+        // Optional: an artifact spec for a protoc plugin, with "grpc" as
+        // the identifier, which can be referred to in the "plugins"
+        // container of the "generateProtoTasks" closure.
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.62.2"
+        }
+    }
+
+    //generatedFilesBaseDir = "$projectDir/src/generated"
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                // Apply the "grpc" plugin whose spec is defined above, without
+                // options. Note the braces cannot be omitted, otherwise the
+                // plugin will not be added. This is because of the implicit way
+                // NamedDomainObjectContainer binds the methods.
+                id("grpc") { }
+            }
+        }
+    }
+
+    // Enable Kotlin generation
+    //generateProtoTasks {
+//        all().forEach {
+//            it.builtins {
+//                id("kotlin")
+//            }
+//        }
+//    }
 }
