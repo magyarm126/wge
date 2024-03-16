0e2d259d3aa7e23c82338e8411daed110e2fec9e
Magyar Máté <magyarmate126@gmail.com>
2024-03-16 21:09:26 +0100

    Hybrid deser works

--- web/backend/src/main/kotlin/hu/matemagyar/wge/proto/serde/ProtoMessageSerdeBean.kt
+++ web/backend/src/main/kotlin/hu/matemagyar/wge/proto/serde/ProtoMessageSerdeBean.kt
@@ -3,21 +3,27 @@ package hu.matemagyar.wge.proto.serde
 
 import com.google.protobuf.Message
 import com.google.protobuf.util.JsonFormat
+import hu.matemagyar.wge.proto.codec.ProtoBufferCodec
 import io.micronaut.core.type.Argument
 import io.micronaut.json.JsonMapper
 import io.micronaut.json.tree.JsonNode
 import io.micronaut.serde.*
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
+import java.util.*
 
 @Singleton
-class ProtoMessageSerdeBean : Serde<Message>{
+class ProtoMessageSerdeBean : Serde<Message> {
 
     @Inject
     lateinit var objectMapper: JsonMapper
 
     @Inject
     lateinit var defaultSerdeRegistry: SerdeRegistry
+
+    @Inject
+    lateinit var codec: ProtoBufferCodec
+
 
     override fun serialize(
         encoder: Encoder?,
@@ -38,35 +44,20 @@ class ProtoMessageSerdeBean : Serde<Message>{
         context: Deserializer.DecoderContext?,
         type: Argument<in Message>?
     ): Message {
-        var a = decoder?.decodeObject(Argument.of(JsonNode::class.java))
-        a.toString()
-        /*if (type is Message) {
-         *
-
-
-            try {
-                val jsonBuilder = getBuilderTyped(type).orElseThrow()
-                JsonFormat.parser().merge(InputStreamReader(inputStream), jsonBuilder)
-                return type.type.cast(jsonBuilder.build())
-            } catch (e: IOException) {
-                throw CodecException("Failed to read protobuf from JSON", e)
-            }
+        if (decoder == null || type == null) {
+            throw RuntimeException("meh")
         }
 
-        val returnList = ArrayList<Message>()
-        val jsonArrayString = inputStream.bufferedReader().use { it.readText() }
-        val jsonArray = JSONArray(jsonArrayString)
-        for (i in 0 until jsonArray.length()) {
-            val jsonObject: JSONObject = jsonArray.getJSONObject(i)
-            val jsonBuilder = getBuilder(type.typeVariables.values.first()).orElseThrow()
-            JsonFormat.parser().merge(jsonObject.toString(), jsonBuilder)
-            val msg: Message = jsonBuilder.build() as Message
-            returnList.addLast(msg)
-        }
-        return type.type.cast(returnList)
-        throw RuntimeException("No deserializer for MessageLite")
-        */
-        return null as Message
+        val rootJson: JsonNode = decoder.decodeNode()
+        val writeValueAsString = objectMapper.writeValueAsString(rootJson)
+        val jsonBuilder = getBuilder(type).orElseThrow()
+        JsonFormat.parser().merge(writeValueAsString, jsonBuilder)
+        return type.type.cast(jsonBuilder.build()) as Message
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun getBuilder(type: Argument<*>): Optional<Message.Builder> {
+        return codec.getMessageBuilder(type.type as Class<out Message?>)
     }
 
 }
